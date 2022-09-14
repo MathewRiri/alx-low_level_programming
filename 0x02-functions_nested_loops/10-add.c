f3606e82cd830a6e110a15ff972e9c6db884999d
MathewRiri <mrnjoka254@gmail.com>
2022-09-14 10:06:15 +0300

    Correction of "10. a + b"

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * add - Adds two integers
  * @num1: first integer to be added 
@@ -7,7 +7,7 @@
  *
  * Return: The result of the addition
  */
-int = add(int num1, int num2)
+int add(int num1, int num2)
 {
 	return (num1 + num2);
 }
