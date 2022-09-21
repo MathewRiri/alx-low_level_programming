a76cac79794fecdd2f241e488398c4edc3643279
MathewRiri <mrnjoka254@gmail.com>
2022-09-21 21:15:12 +0300

    njoka

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,10 +2,6 @@
 
 /**
 *string_toupper - changes all lowercases letters of a string to uppercase
-#include "main.h"
-
-/**
-*string_toupper - changes all lowercases letters of a string to uppercase
 *@str: parameter
 *Return: returns a character
 */
